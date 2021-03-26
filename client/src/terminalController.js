b26f9b09923dd8e35ca3b2839ce779fc8af4cb47
Vinicius Melo <vmelo@gvdasa.com.br>
2021-03-26 19:34:49 -0300

    feat: initial commit, config chat structure

---
+++ client/src/terminalController.js
@@ -0,0 +1,106 @@
+import ComponentsBuilder from "./component.js";
+import { constants } from './constants.js';
+
+export default class TerminalController {
+    #usersCollors = new Map();
+
+    constructor() { }
+
+    // randomic RGB
+    #pickCollor() {
+        return `#${((1 << 24) * Math.random() | 0).toString(16)}-fg`
+    }
+
+    #getUserCollor(userName) {
+        if (this.#usersCollors.has(userName)) {
+            return this.#usersCollors.get(userName);
+        }
+
+        const collor = this.#pickCollor();
+        this.#usersCollors.set(userName, collor);
+
+        return collor;
+    }
+
+
+    #onInputReceived(eventEmitter) {
+        return function () {
+            const message = this.getValue();
+            console.log(message);
+            this.clearValue();
+        }
+    }
+
+    #onMessageReceived({ screen, chat }) {
+        return msg => {
+            const { userName, message } = msg;
+            const collor = this.#getUserCollor(userName);
+
+            chat.addItem(`{${collor}}{bold}${userName}{/}: ${message}`);
+
+            screen.render();
+        }
+    }
+
+    #onLogChanged({ screen, activityLog }) {
+        return msg => {
+            const [userName] = msg.split(/\s/); // regex for != white space
+            const collor = this.#getUserCollor(userName);
+
+            activityLog.addItem(`{${collor}}{bold}${msg.toString()}{/}`);
+
+            screen.render();
+        }
+    }
+
+    #onStatusChange({ screen, status }) {
+        return users => {
+            // take the first element
+            const { content } = status.items.shift();
+
+            status.clearItems();
+            status.addItem(content);
+
+            users.forEach(userName => {
+                const collor = this.#getUserCollor(userName);
+                status.addItem(`{${collor}}{bold}${userName}{/}`);
+            });
+
+            screen.render();
+        }
+    }
+
+    #registerEvents(eventEmitter, components) {
+        eventEmitter.on(constants.events.app.MESSAGE_RECEIVED, this.#onMessageReceived(components));
+        eventEmitter.on(constants.events.app.ACTIVITYLOG_UPDATED, this.#onLogChanged(components));
+        eventEmitter.on(constants.events.app.STATUS_UPDATED, this.#onStatusChange(components));
+    }
+
+    async initializeTable(eventEmitter) {
+        const components = new ComponentsBuilder()
+            .setScreen({ title: 'HackerChat - Vinicius Melo' })
+            .setLayoutComponent()
+            .setInputComponent(this.#onInputReceived(eventEmitter))
+            .setChatComponent()
+            .setActivityLogComponent()
+            .setStatusComponent()
+            .build();
+
+        this.#registerEvents(eventEmitter, components);
+
+        components.input.focus();
+        components.screen.render();
+
+        // setInterval(() => {
+        const users = ['vinishow'];
+
+        eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+        users.push('teste');
+        eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+        users.push('teste1', 'teste2');
+        eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+        users.push('teste3', 'teste4');
+        eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+        // }, 1000);
+    }
+}
